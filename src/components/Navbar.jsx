bed882ed41e3203efee62ac6e70cfa6c9ed1ca68
JCesar206 <jcesar206@hotmail.com>
2025-10-02 14:49:57 -0600

    Se actualizan varios componentes en estilo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-{/* Componente Navbar */}
 import React, { useState, useContext } from "react";
 import { FaMoon, FaSun, FaBars, FaTimes } from "react-icons/fa";
 import { LangContext, ThemeContext } from "../App";
