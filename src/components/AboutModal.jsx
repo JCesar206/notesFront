f7b0cc004a7b1afe66674a3c040e00af4bfe7542
JCesar206 <jcesar206@hotmail.com>
2025-10-02 13:59:08 -0600

    Se actualizan componentes AboutModal y AddNote

--- src/components/AboutModal.jsx
+++ src/components/AboutModal.jsx
@@ -12,9 +12,9 @@ function AboutModal({ close }) {
   }[lang];
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 p-4">
-      <div className="bg-white dark:bg-gray-800 rounded shadow max-w-md w-full p-6 text-center">
-        <img src="myPhoto.jpg" alt="Foto Profesional" className="w-32 h-32 object-cover rounded-full mx-auto mb-4" />
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 p-4 rounded">
+      <div className="bg-white dark:bg-gray-800 rounded border-0 border-b-purple-400 shadow max-w-md w-full p-6 text-center">
+        <img src="./myPhoto.jpg" alt="Foto Profesional" className="w-32 h-32 object-cover rounded-full mx-auto mb-4" />
         <h2 className="text-xl font-bold mb-2">{t.title}</h2>
         <p className="mb-4">{t.text}</p>
         <div className="mb-4">
